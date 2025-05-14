ba27f64cc6ed5c2249247451da3da4f59774f1d1
pundoun <nnquynhndnq@gmail.com>
2025-05-15 00:12:20 +0700

    update app

--- BE/src/routes/delivery_address.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllDeliveryAddressesByUser, addNewDeliveryAddress } from "../controllers/delivery_address.js";
-
-const router = express.Router();
-
-router.post("/add-delivery-address", addNewDeliveryAddress);
-router.get("/get-delivery-addresses-by-user/:userId", getAllDeliveryAddressesByUser)
-
-export default router;
